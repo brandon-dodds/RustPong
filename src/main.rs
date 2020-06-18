41136ae5d210049ecbb4203b67ee7f6a575e563a
Brandon Dodds <brandon.lee.dodds@gmail.com>
2020-06-18 10:34:29 +0100

    Added comments.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use ggez::input::keyboard;
 use ggez::input::keyboard::KeyCode;
 use ggez::*;
 
+//The main state of the program (players, balls).
 struct MainState {
     player1_coord: Vector2<f32>,
     player2_coord: Vector2<f32>,
     ball_coord: Point2<f32>,
 }
-
+//Implements the main state. New is created on creation of the game state.
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let player1_coord = Vector2::new(100.0, 285.0);
@@ -25,7 +26,7 @@ impl MainState {
 
         Ok(s)
     }
-
+    //Resetting the ball position/Handling the physics of ball.
     fn ball_update_position(&mut self) {
         self.ball_coord = Point2::new(640.0, 360.0);
     }
@@ -47,7 +48,7 @@ impl event::EventHandler for MainState {
             self.player2_coord.y += 10.0
         }
         //Ball code.
-        self.ball_coord += Vector2::new(-2.0, 0.0);
+        self.ball_coord += Vector2::new(2.0, 0.0);
         if (self.ball_coord.x == self.player1_coord.x
             || self.ball_coord.x == self.player1_coord.x + 10.0)
             && self.ball_coord.y >= self.player1_coord.y
@@ -67,9 +68,9 @@ impl event::EventHandler for MainState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, graphics::BLACK);
-        let line_char1 =
+        let rectangle_for_player_1 =
             graphics::Rect::new(self.player1_coord.x, self.player1_coord.y, 10.0, 150.0);
-        let line_char2 =
+        let rectangle_for_player_2 =
             graphics::Rect::new(self.player2_coord.x, self.player2_coord.y, 10.0, 150.0);
         let ball = graphics::Mesh::new_circle(
             ctx,
@@ -82,17 +83,17 @@ impl event::EventHandler for MainState {
         let r1 = graphics::Mesh::new_rectangle(
             ctx,
             graphics::DrawMode::fill(),
-            line_char1,
+            rectangle_for_player_1,
             graphics::WHITE,
         )?;
 
         let r2 = graphics::Mesh::new_rectangle(
             ctx,
             graphics::DrawMode::fill(),
-            line_char2,
+            rectangle_for_player_2,
             graphics::WHITE,
         )?;
-
+        //Draw graphics to screen.
         graphics::draw(ctx, &r1, DrawParam::default())?;
         graphics::draw(ctx, &r2, DrawParam::default())?;
         graphics::draw(ctx, &ball, DrawParam::default())?;
@@ -103,12 +104,14 @@ impl event::EventHandler for MainState {
 }
 
 pub fn main() -> GameResult {
+    //Main context and event loop creation.
     let cb = ggez::ContextBuilder::new("Game1", "Brandon");
     let (ctx, events_loop) = &mut cb.build()?;
     let state = &mut MainState::new(ctx).unwrap();
-
+    //Makes the window the right size.
     graphics::set_drawable_size(ctx, 1280.0, 720.0).unwrap();
     graphics::set_screen_coordinates(ctx, graphics::Rect::new(0.0, 0.0, 1280.0, 720.0)).unwrap();
     graphics::set_window_title(ctx, "Rust Pong!");
+    //Let us start the game!
     event::run(ctx, events_loop, state)
 }

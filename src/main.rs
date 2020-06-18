256674984e3fef2c995421fc0431aaf51fe452b0
Brandon Dodds <brandon.lee.dodds@gmail.com>
2020-06-18 17:51:50 +0100

    Added the ball struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use cgmath;
 use cgmath::{Point2, Vector2};
 use ggez::graphics::DrawParam;
@@ -5,25 +7,34 @@ use ggez::input::keyboard;
 use ggez::input::keyboard::KeyCode;
 use ggez::*;
 
+//The ball struct.
+
+struct Ball {
+    ball_coord: Point2<f32>,
+    ball_movement: Vector2<f32>,
+    ball_speed: f32,
+}
+
 //The main state of the program (players, balls).
 struct MainState {
     player1_coord: Vector2<f32>,
     player2_coord: Vector2<f32>,
-    ball_coord: Point2<f32>,
-    ball_movement: Vector2<f32>,
+    used_ball: Ball,
 }
 //Implements the main state. New is created on creation of the game state.
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let player1_coord = Vector2::new(100.0, 285.0);
         let player2_coord = Vector2::new(1170.0, 285.0);
-        let ball_coord = Point2::new(640.0, 360.0);
-        let ball_movement = Vector2::new(-5.0, 0.0);
+        let used_ball = Ball {
+            ball_coord: Point2::new(640.0, 360.0),
+            ball_movement: Vector2::new(-5.0, 0.0),
+            ball_speed: 3.0,
+        };
         let s = MainState {
             player1_coord,
             player2_coord,
-            ball_coord,
-            ball_movement,
+            used_ball,
         };
 
         Ok(s)
@@ -33,25 +44,29 @@ impl MainState {
     // Use trigonometry to send the ball at an angle according to this difference.
     fn ball_update_position(&mut self, player_val: i8) {
         let degree: f32 = 75.0;
-        let speed = 5.0;
+        if self.used_ball.ball_speed < 12.0 {
+            self.used_ball.ball_speed += 1.0;
+        }
+        let speed = self.used_ball.ball_speed;
+        println!("{}", speed);
         if player_val == 1 {
             let paddle_middle = (self.player1_coord.y * 2.0 + 150.0) / 2.0;
-            let diff = paddle_middle - self.ball_coord.y;
+            let diff = paddle_middle - self.used_ball.ball_coord.y;
             let normalized_value: f32 = diff / ((self.player1_coord.y + 150.0) / 2.0);
             let bounce_angle = normalized_value * degree.to_radians();
-            self.ball_movement.x = speed * bounce_angle.cos();
-            self.ball_movement.y = speed * -bounce_angle.sin();
+            self.used_ball.ball_movement.x = speed * bounce_angle.cos();
+            self.used_ball.ball_movement.y = speed * -bounce_angle.sin();
         } else if player_val == 2 {
             let paddle_middle = (self.player2_coord.y * 2.0 + 150.0) / 2.0;
-            let diff = paddle_middle - self.ball_coord.y;
+            let diff = paddle_middle - self.used_ball.ball_coord.y;
             let normalized_value: f32 = diff / ((self.player2_coord.y + 150.0) / 2.0);
             let bounce_angle: f32 = normalized_value * degree.to_radians();
-            self.ball_movement.x = speed * -bounce_angle.cos();
-            self.ball_movement.y = speed * -bounce_angle.sin();
+            self.used_ball.ball_movement.x = speed * -bounce_angle.cos();
+            self.used_ball.ball_movement.y = speed * -bounce_angle.sin();
         } else if player_val == 3 {
-            self.ball_movement.y = speed * -degree.sin();
+            self.used_ball.ball_movement.y = speed * -degree.sin();
         } else if player_val == 4 {
-            self.ball_movement.y = speed * degree.sin();
+            self.used_ball.ball_movement.y = speed * degree.sin();
         }
     }
 }
@@ -72,31 +87,31 @@ impl event::EventHandler for MainState {
             self.player2_coord.y += 10.0
         }
         //Ball code.
-        self.ball_coord += self.ball_movement;
-        if self.ball_coord.x >= self.player1_coord.x
-            && self.ball_coord.x <= self.player1_coord.x + 10.0
-            && self.ball_coord.y >= self.player1_coord.y
-            && self.ball_coord.y <= self.player1_coord.y + 150.0
+        self.used_ball.ball_coord += self.used_ball.ball_movement;
+        if self.used_ball.ball_coord.x >= self.player1_coord.x
+            && self.used_ball.ball_coord.x <= self.player1_coord.x + 10.0
+            && self.used_ball.ball_coord.y >= self.player1_coord.y
+            && self.used_ball.ball_coord.y <= self.player1_coord.y + 150.0
         {
             self.ball_update_position(1);
         }
-        if self.ball_coord.x >= self.player2_coord.x
-            && self.ball_coord.x <= self.player2_coord.x + 10.0
-            && self.ball_coord.y >= self.player2_coord.y
-            && self.ball_coord.y <= self.player2_coord.y + 150.0
+        if self.used_ball.ball_coord.x >= self.player2_coord.x
+            && self.used_ball.ball_coord.x <= self.player2_coord.x + 10.0
+            && self.used_ball.ball_coord.y >= self.player2_coord.y
+            && self.used_ball.ball_coord.y <= self.player2_coord.y + 150.0
         {
             self.ball_update_position(2);
         }
-        if self.ball_coord.y <= 0.0 {
+        if self.used_ball.ball_coord.y <= 0.0 {
             self.ball_update_position(3);
         }
-        if self.ball_coord.y >= graphics::drawable_size(ctx).1 {
+        if self.used_ball.ball_coord.y >= graphics::drawable_size(ctx).1 {
             self.ball_update_position(4);
         }
-        if self.ball_coord.x <= 0.0 {
+        if self.used_ball.ball_coord.x <= 0.0 {
             ggez::event::quit(ctx);
         }
-        if self.ball_coord.x >= graphics::drawable_size(ctx).0 {
+        if self.used_ball.ball_coord.x >= graphics::drawable_size(ctx).0 {
             ggez::event::quit(ctx);
         }
         Ok(())
@@ -111,7 +126,7 @@ impl event::EventHandler for MainState {
         let ball = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            self.ball_coord,
+            self.used_ball.ball_coord,
             10.0,
             2.0,
             graphics::WHITE,
